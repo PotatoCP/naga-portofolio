77d7e1e394d1a9c1655e26e3c2b2c6f00c50c8f5
PotatoCP <kenzieradityatirtarahardja18@gmail.com>
2025-07-20 23:21:46 +0800

    Add sound for social media button

--- src/components/social-media-item.tsx
+++ src/components/social-media-item.tsx
@@ -1,11 +1,27 @@
+"use client";
+
 interface SocialMediaItemProps {
   href: string;
   iconClass: string;
 }
 
-export default function SocialMediaItem({ href, iconClass }: SocialMediaItemProps) {
+export default function SocialMediaItem({
+  href,
+  iconClass,
+}: SocialMediaItemProps) {
+  function playSound() {
+    const snd = new Audio("button-heavy.mp3");
+    snd.play();
+  }
+
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" className="mr-4">
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="mr-4"
+      onClick={playSound}
+    >
       <i className={iconClass}></i>
     </a>
   );
--- src/components/social-media.tsx
+++ src/components/social-media.tsx
@@ -1,11 +1,20 @@
-import SocialMediaItem from "./social-media-item"
+import SocialMediaItem from "./social-media-item";
 
 export default function SocialMedia() {
   return (
     <div>
-      <SocialMediaItem href="https://github.com/PotatoCP" iconClass="nes-icon github is-medium" />
-      <SocialMediaItem href="https://www.linkedin.com/in/kenzie-raditya-tirtarahardja-b93760192/" iconClass="nes-icon linkedin is-medium" />
-      <SocialMediaItem href="https://www.instagram.com/ken_murd/" iconClass="nes-icon instagram is-medium" />
+      <SocialMediaItem
+        href="https://github.com/PotatoCP"
+        iconClass="nes-icon github is-medium"
+      />
+      <SocialMediaItem
+        href="https://www.linkedin.com/in/kenzie-raditya-tirtarahardja-b93760192/"
+        iconClass="nes-icon linkedin is-medium"
+      />
+      <SocialMediaItem
+        href="https://www.instagram.com/ken_murd/"
+        iconClass="nes-icon instagram is-medium"
+      />
     </div>
   );
 }
